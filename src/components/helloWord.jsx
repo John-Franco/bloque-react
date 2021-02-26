6c9892db244afe200d647fcfe9039add186b8c83
johngonzalez-gif <58112957+johngonzalez-gif@users.noreply.github.com>
2021-02-26 13:10:09 -0500

    configuracion basica React

--- src/components/helloWord.jsx
+++ src/components/helloWord.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const HelloWordView = () => {
-	return <h1>Hola Mundo Se acutaliza el cambio en tiempo real</h1>;
-};
+const HelloWordView = () => <h1>Hola Mundo Se acutaliza el cambio en tiempo real</h1>;
 
 export default HelloWordView;
