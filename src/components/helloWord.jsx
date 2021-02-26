af4f2b9425730ec7cd92986cc3d79cf7dc0e007b
johngonzalez-gif <58112957+johngonzalez-gif@users.noreply.github.com>
2021-02-26 12:43:04 -0500

    Configuracion Basica React

---
+++ src/components/helloWord.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloWordView = () => {
+	return <h1>Hola Mundo Se acutaliza el cambio en tiempo real</h1>;
+};
+
+export default HelloWordView;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import HelloWordView from "./components/helloWord";
+
+ReactDOM.render(<HelloWordView />, document.getElementById("app"));
